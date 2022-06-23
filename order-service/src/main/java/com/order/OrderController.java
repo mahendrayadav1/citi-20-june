1d4162de3d68e2a2ae5b19c0c9f10e22facd933d
ibmpariwesh <pariweshg@gmail.com>
2022-06-23 10:29:37 +0530

    handling error

--- order-service/src/main/java/com/order/OrderController.java
+++ order-service/src/main/java/com/order/OrderController.java
@@ -1,13 +1,21 @@
 package com.order;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +24,21 @@ public class OrderController {
 	@Autowired
 	OrderService orderService;
 	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	Map<String, String> handleValidationErrors(MethodArgumentNotValidException exception){
+		Map<String, String> errorMessages= new HashMap<>();
+		exception.getAllErrors().forEach(error-> {
+			String fieldName = ((FieldError) error).getField();
+			String message = ((FieldError) error).getDefaultMessage();
+			errorMessages.put(fieldName, message);
+			
+		});
+		return errorMessages;
+	}
+	
 	@PostMapping
+	@ResponseStatus(code=HttpStatus.CREATED)
 	public void createOrder(@Valid @RequestBody OrderVO order) {
 		orderService.insertOrder(order);
 		System.out.println(order.getPrice());
--- order-service/src/main/java/com/order/OrderVO.java
+++ order-service/src/main/java/com/order/OrderVO.java
@@ -7,7 +7,12 @@ import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
 @Entity
+@Setter
+@Getter
 public class OrderVO {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,44 +25,4 @@ public class OrderVO {
 	private String email;
 	@Max(value = 10)
 	private int quantity;
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getItem() {
-		return item;
-	}
-
-	public void setItem(String item) {
-		this.item = item;
-	}
-
-	public float getPrice() {
-		return price;
-	}
-
-	public void setPrice(float price) {
-		this.price = price;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
 }
